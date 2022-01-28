1562cfb309ca2c1c61aaffdc4ad5b758792e38ed
RudnevI <irudnev@protonmail.com>
2022-01-28 14:21:50 +0600

    add tag system

--- scripts/article-list.js
+++ scripts/article-list.js
@@ -1,7 +1,68 @@
 const rootUrl = "http://127.0.0.1:8001";
 
 let fetchedData;
+const deleteIconHtml = `<i class="fa fa-trash" aria-hidden="true"></i>`;
+const editIconHtml = `<i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+    `;
+const notificationServiceRootUrl = "http://127.0.0.1:8002";
 
+let templateId;
+
+const notifyOfDeletion = (template_id) => {
+  fetch(`${notificationServiceRootUrl}/api/notifications/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      notification: {
+        params: {
+          PARAMS1: "abdulla123123123123",
+        },
+        sendMethodID_id: 2,
+        templateID_id: template_id,
+      },
+    }),
+  });
+};
+
+const getTagsByArticleId = (articleId) => {
+  let tags;
+  fetch(`${rootUrl}/tags/`)
+    .then((response) => response.json())
+    .then((data) => {
+      tags = Array.from(data)
+        .filter((tag) => tag.article_id.includes(articleId))
+        .map((tag) => `#${tag.tag_name}`)
+        .join(" ");
+    });
+  return tags;
+};
+
+let tagArticleMap = new Map();
+
+const createNotificationTemplate = async (articleId, title) => {
+  await fetch(`${notificationServiceRootUrl}/api/templates`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      template: {
+        name: "Уведомление об удалении статьи",
+        text: `Статья под названием ${title} была удалена`,
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      templateId = data.id;
+      console.log(templateId);
+      notifyOfDeletion(data.id);
+    });
+};
 const getArticles = async () => {
   await fetch(`${rootUrl}/articles/`)
     .then((response) => {
@@ -11,18 +72,57 @@ const getArticles = async () => {
       fetchedData = data;
       console.log(fetchedData);
       fetchedData.forEach((element) => {
-        let listElement = document.createElement("div");
-        listElement.classList.add("list-element");
+        let tdButtons = document.createElement("td");
 
-        let idDiv = document.createElement("div");
-        let titleDiv = document.createElement("div");
-        let contentDiv = document.createElement("div");
-        let categoryIdDiv = document.createElement("div");
-        let articleStatusDiv = document.createElement("div");
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn", "btn-danger");
+        deleteButton.innerHTML = deleteIconHtml;
+
+        let editButton = document.createElement("button");
+        editButton.classList.add("btn", "btn-success");
+        editButton.innerHTML = editIconHtml;
+
+        [editButton, deleteButton].forEach((btn) => tdButtons.appendChild(btn));
+
+        editButton.id = element.id;
+        deleteButton.id = element.id;
+
+        editButton.style.marginRight = "1vw";
+
+        let associatedTags = getTagsByArticleId(element.id);
+        tagArticleMap.set(element.id, associatedTags);
+
+        editButton.addEventListener("click", function () {
+          location.replace(`./update-article.html?id=${this.id}`);
+        });
+        deleteButton.name = element.title;
+        deleteButton.addEventListener("click", function () {
+          fetch(`${rootUrl}/articles/${this.id}`, {
+            method: "DELETE",
+          }).then((response) => {
+            if (response.ok) {
+              this.parentElement.parentElement.remove();
+            }
+          });
+          createNotificationTemplate(this.id, this.name);
+        });
+
+        let tr = document.createElement("tr");
+
+        let idDiv = document.createElement("td");
+
+        let articleLink = document.createElement("a");
+        articleLink.href = `./article.html?id=${element.id}`;
+        articleLink.innerText = element.title;
+
+        let titleDiv = document.createElement("td");
+        let contentDiv = document.createElement("td");
+        let categoryIdDiv = document.createElement("td");
+        let articleStatusDiv = document.createElement("td");
 
         idDiv.innerHTML = element.id;
-        titleDiv.innerHTML = element.title;
-        contentDiv.innerHTML = element.content;
+        titleDiv.appendChild(articleLink);
+        contentDiv.innerHTML = element.content.split(" ").slice(0, 5);
         categoryIdDiv.innerHTML = element.category.name;
         articleStatusDiv.innerHTML = element.article_status;
 
@@ -32,12 +132,12 @@ const getArticles = async () => {
           contentDiv,
           categoryIdDiv,
           articleStatusDiv,
-        ].forEach((div) => listElement.appendChild(div));
-        document
-          .getElementsByClassName("main-container")[0]
-          .appendChild(listElement);
+          tdButtons,
+        ].forEach((div) => tr.appendChild(div));
+        document.querySelector("tbody").appendChild(tr);
       });
     });
 };
 
 getArticles();
+console.log(tagArticleMap);
--- scripts/post-article.js
+++ scripts/post-article.js
@@ -1,8 +1,82 @@
 const rootUrl = "http://127.0.0.1:8001";
 
+const getAllTags = () => {
+  let tagSelect = document.querySelector("#tags");
+  fetch(`${rootUrl}/tags/`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element) => {
+        let option = document.createElement("option");
+        option.value = element.id;
+        option.innerHTML = element.tag_name;
+        tagSelect.appendChild(option);
+      });
+    });
+};
+
+getAllTags();
+
+const notificationServiceRootUrl = "http://127.0.0.1:8002";
+
+let templateId;
+
+const notifyOfCreation = (template_id) => {
+  fetch(`${notificationServiceRootUrl}/api/notifications/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      notification: {
+        params: {
+          PARAMS1: "abdulla123123123123",
+        },
+        sendMethodID_id: 2,
+        templateID_id: template_id,
+      },
+    }),
+  });
+};
+
+const createNotificationTemplate = (title) => {
+  fetch(`${notificationServiceRootUrl}/api/templates`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      template: {
+        name: "Уведомление о создании статьи",
+        text: `Новая статья: ${title}\n${getSelectedTagsNames()}`,
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      templateId = data.id;
+      console.log(templateId);
+      notifyOfCreation(templateId);
+    });
+};
+
+const getSelectedTags = () => {
+  return Array.from(document.querySelector("#tags").children).filter(
+    (child) => child.selected
+  );
+};
+
+const getSelectedTagsNames = () => {
+  return getSelectedTags()
+    .map((tag) => `#${tag.innerHTML}`)
+    .join(" ");
+};
+
 const postArticleButton = document.getElementById("postArticleButton");
 const postArticle = async () => {
-  let rawResponse = await fetch(`${rootUrl}/articles/`, {
+  let id;
+  await fetch(`${rootUrl}/articles/`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -14,8 +88,41 @@ const postArticle = async () => {
       category: document.getElementsByName("category")[0].value,
       article_status: document.getElementsByName("articleStatus")[0].value,
     }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        createNotificationTemplate(
+          document.getElementsByName("title")[0].value
+        );
+      }
+      return response.json();
+    })
+
+    .then((data) => {
+      id = data.id;
+    });
+
+  getSelectedTags().forEach((child) => {
+    console.log({
+      tag_name: child.innerHTML,
+      article_id: [
+        {
+          title: document.getElementsByName("title")[0].value,
+        },
+      ],
+    });
+    fetch(`${rootUrl}/tags/${child.value}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tag_name: child.innerHTML,
+        article_id: [id],
+      }),
+    });
   });
-  console.log(rawResponse.json());
 };
 
 postArticleButton.onclick = postArticle;
@@ -43,16 +150,3 @@ const fillCategoryDropdown = async () => {
 };
 
 fillCategoryDropdown();
-
-document.querySelector("#addTagButton").addEventListener("click", () => {
-  let tagFieldToInsert = document.createElement("input");
-  tagFieldToInsert.setAttribute("type", "text");
-  tagFieldToInsert.setAttribute("name", "articleTag");
-
-  document
-    .getElementsByName("articleTag")
-    [document.getElementsByName("articleTag").length - 1].insertAdjacentElement(
-      "afterend",
-      tagFieldToInsert
-    );
-});
